c2aaa3d01241b71d37e81fefcf4361f7a14b4953
Domonkos Nagy <nagydomonkos1@gmail.com>
2022-07-30 22:45:08 +0200

    -

--- configure.sh
+++ configure.sh
@@ -9,18 +9,21 @@ rm -f ~/.config/bspwm/bspwmrc
 rm -f ~/.config/sxhkd/sxhkdrc
 rm -f ~/.config/polybar/config.ini
 sudo rm -f /etc/default/grub
+rm -f ~/.config/rofi/config.rasi
 
 # Create symlinks
 
 ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/urxvt/.Xresources ~/.Xresources
-mkdir -p .config/{bspwm,sxhkd,polybar}
+mkdir -p .config/{bspwm,sxhkd,polybar,picom,rofi}
 ln -s ~/.dotfiles/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 ln -s ~/.dotfiles/bspwm/sxhkdrc ~/.config/sxhkd/sxhkdrc
 ln -s ~/.dotfiles/polybar/config.ini ~/.config/polybar/config.ini
 sudo ln -s ~/.dotfiles/system/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo ln -s ~/.dotfiles/system/grub /etc/default/grub
+ln -s ~/.dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -s ~/.dotfiles/system/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
 # Additional configuration
 
@@ -28,3 +31,6 @@ sudo systemctl enable ly.service
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
 echo 'set completion-ignore-case On' >> ~/.inputrc
+sudo gpasswd -a $USER input
+newgrp input
+libinput-gestures-setup autostart start
--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 
 sudo reflector -c Hungary -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
-sudo pacman -S bspwm sxhkd firefox rxvt-unicode ttf-jetbrains-mono archlinux-keyring bluez bluez-utils alsa-utils pulseaudio rsync networkmanager network-manager-applet xorg git base-devel polybar feh picom ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-linux-libertine ttf-dejavu nm-connection-editor ranger dunst
-yay -S ly otf-nerd-fonts-fira-code ksuperkey
+sudo pacman -S bspwm sxhkd firefox rxvt-unicode ttf-jetbrains-mono archlinux-keyring bluez bluez-utils alsa-utils pulseaudio rsync networkmanager network-manager-applet xorg git base-devel polybar feh picom ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-linux-libertine ttf-dejavu nm-connection-editor ranger dunst xdotool wmctrl
+yay -S ly otf-nerd-fonts-fira-code ksuperkey libinput-gestures
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
