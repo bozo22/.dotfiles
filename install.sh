bc915ec9ef7ecc2cf3a287a06497afc04d71e24b
Domonkos Nagy <nagydomonkos1@gmail.com>
2022-07-30 21:46:16 +0200

    -

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo reflector -c Hungary -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo pacman -S bspwm sxhkd firefox rxvt-unicode ttf-jetbrains-mono archlinux-keyring bluez bluez-utils alsa-utils pulseaudio rsync networkmanager network-manager-applet xorg git base-devel polybar feh picom ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-linux-libertine ttf-dejavu nm-connection-editor ranger dunst
-yay -S ly otf-nerd-fonts-fira-code ksuperkey rofi-bluetooth-git
+yay -S ly otf-nerd-fonts-fira-code ksuperkey
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
