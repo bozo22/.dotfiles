970e7ee9e068638052a3cc0f25c6d3acd85da80e
Domonkos Nagy <nagydomonkos1@gmail.com>
2022-08-03 19:20:30 +0200

    -

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,5 +7,6 @@ killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar second 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
