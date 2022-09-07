45161c3ec039381f4b8a9e9f35e9f2c9512b0232
Domonkos Nagy <nagydomonkos1@gmail.com>
2022-09-07 13:57:40 +0200

    -

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 sudo reflector -c Hungary -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
-sudo pacman -S bspwm sxhkd firefox rxvt-unicode ttf-jetbrains-mono archlinux-keyring bluez bluez-utils alsa-utils pulseaudio rsync networkmanager network-manager-applet xorg git base-devel polybar feh picom ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-linux-libertine ttf-dejavu nm-connection-editor ranger dunst xdotool wmctrl zsh iw zip unzip nomacs neovim pulseaudio-alsa kitty zathura zathura-pdf-mupdf python-pip highlight xclip htop ncdu optipng scrot papirus-icon-theme
+sudo pacman -S bspwm sxhkd firefox rxvt-unicode ttf-jetbrains-mono archlinux-keyring bluez bluez-utils alsa-utils pulseaudio rsync networkmanager network-manager-applet xorg git base-devel polybar feh picom ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-linux-libertine ttf-dejavu nm-connection-editor ranger dunst xdotool wmctrl zsh iw zip unzip nomacs neovim pulseaudio-alsa kitty zathura zathura-pdf-mupdf python-pip highlight xclip htop ncdu optipng scrot papirus-icon-theme rsync xf86-video-intel rofi
 yay -S ly otf-nerd-fonts-fira-code ksuperkey libinput-gestures nitch hardcode-tray
 pip install pillow
 
