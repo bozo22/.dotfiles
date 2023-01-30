9f06175aaf2de5314bcdadc2e57e9ce1ed4ff8b6
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2023-01-30 19:25:59 +0000

    -

--- scripts/link.sh
+++ scripts/link.sh
@@ -7,7 +7,6 @@ sudo rm -f /etc/bluetooth/main.conf
 rm -rf ~/.config/{bspwm,sxhkd,polybar,rofi,picom,kitty,nvim,ranger,dunst,zathura,zsh,awesome}
 
 # Create symlinks
-
 ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
@@ -16,6 +15,5 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.ghc ~/.ghc
 sudo ln -s ~/.dotfiles/x/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 ln -s ~/.dotfiles/{bspwm,sxhkd,polybar,rofi,picom,kitty,nvim,ranger,dunst,zathura,zsh,awesome} ~/.config
-
-# Additional config
+test
 
