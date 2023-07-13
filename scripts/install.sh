f071f08306960e46f9819944e58c725186701ba9
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2023-07-13 20:59:10 +0200

    -

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,81 +1,35 @@
-#!/bin/bash
-
-distros=("arch")
-levels=("full" "minimal")
-yn=("y" "n")
-
-# Setup
-
-echo "Install system packages? (y/n)"
-while read -p '> ' system && [[ ! " ${yn[*]} " =~ " ${system} " ]]
-do
-    echo "Answer y/n"
-done
-
-if [ "$system" = "y" ]
-then
-    echo "Choose your distro:"
-    while read -p '> ' distro && [[ ! " ${distros[*]} " =~ " ${distro} " ]]
-    do
-        echo "Enter a valid distro. Possible choices: $distros"
-    done
-
-    echo "Choose the installation level (full/minimal):"
-    while read -p '> ' level && [[ ! " ${levels[*]} " =~ " ${level} " ]]
-    do
-        echo "Enter full or minimal."
-    done
-fi
-
-echo "Install and configure Haskell? (y/n)"
-while read -p '> ' haskell && [[ ! " ${yn[*]} " =~ " ${haskell} " ]]
-do
-    echo "Answer y/n"
-done
-
-if [ "$haskell" = "y" ]
-then
-    echo "Install and configure Agda? (y/n)"
-    while read -p '> ' agda && [[ ! " ${yn[*]} " =~ " ${agda} " ]]
-    do
-        echo "Answer y/n"
-    done
-fi
+#!/bin/sh
 
 # Installation
 
-if [ "$system" = "y" ]
-then
-    if [ "$distro" = "arch" ]
+install () {
+    package_manager=$1
+    package_list=$2
+    if [ "$package_manager" = "pacman" ]
     then
-        chmod +x $HOME/.dotfiles/install/arch/install.sh
-        $HOME/.dotfiles/install/arch/install.sh $level
-    fi
-
-    if [ "$level" = "full" ]
+        while read -r line;
+        do
+            sudo pacman -S --needed --noconfirm $line
+        done < $package_list
+    elif [ "$package_manager" = "yay" ]
     then
-        git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d && ~/.emacs.d/bin/doom install # doom emacs
+        while read -r line;
+        do
+            yay -S --needed --noconfirm $line
+        done < $package_list
     fi
+}
 
-    pip install pillow # ranger image preview
-fi
+sudo pacman -S --needed git base-devel reflector && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ..
+sudo rm -r yay
+yay -Syyuu
+sudo reflector -c Hungary -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
-if [ "$haskell" = "y" ]
-then
-    ghcup install ghc 9.4.4
-    ghcup set ghc 9.4.4
-    ghcup install cabal
-    ghcup set cabal
-    cabal update
+install pacman $HOME/.dotfiles/res/pkglist/pkglist.txt
+install yay $HOME/.dotfiles/res/pkglist/pkglist_aur.txt
 
-    if [ "$agda" = "y" ]
-    then
-        cabal install Agda
-    fi
-fi
-
-
-chmod +x $HOME/.dotfiles/install/link.sh
-chmod +x $HOME/.dotfiles/install/configure.sh
-$HOME/.dotfiles/install/link.sh
-$HOME/.dotfiles/install/configure.sh
+chmod +x cleanup.sh
+chmod +x link.sh
+$HOME/.dotfiles/scripts/cleanup.sh
+$HOME/.dotfiles/scripts/link.sh
