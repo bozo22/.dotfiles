544d4f52911f2607c5c064aef64e78c6737ef37d
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2023-07-15 20:34:05 +0000

    -

--- dunst/scripts/changebrightness.sh
+++ dunst/scripts/changebrightness.sh
@@ -5,10 +5,10 @@
 msgTag="brightness"
 
 # Change the volume using alsa(might differ if you use pulseaudio)
-xbacklight "$@" > /dev/null
+brightnessctl "$@" > /dev/null
 
 # Query amixer for the current volume and whether or not the speaker is muted
-brightness="$(xbacklight | tr "." "\n" | head -1)"
+brightness="$(light | tr "." "\n" | head -1)"
     # Show the brighness notification
     dunstify -a "changeBrightness" -u low -h string:x-dunst-stack-tag:$msgTag \
     -h int:value:"$brightness" "Brightness: ${brightness}%" -t 1000
--- dunst/scripts/changevolume.sh
+++ dunst/scripts/changevolume.sh
@@ -10,7 +10,7 @@ amixer set Master "$@" > /dev/null
 # Query amixer for the current volume and whether or not the speaker is muted
 volume="$(amixer get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
 mute="$(amixer get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
-if [[ $volume == 0 || "$mute" == "off" ]]; then
+if [[ "$mute" == "off" ]]; then
     # Show the sound muted notification
     dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted" -t 1000
 else
