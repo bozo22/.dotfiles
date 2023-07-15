2e1159a816a247023fe0d764a45485e90b90a28a
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2023-07-15 21:09:32 +0200

    -

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,14 +20,27 @@ install () {
     fi
 }
 
-sudo pacman -S --needed git base-devel reflector && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-cd ..
-sudo rm -r yay
+# yay
+
+if ! yay --version &> /dev/null
+then
+    sudo pacman -S --needed git base-devel reflector && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    cd ..
+    sudo rm -r yay
+fi
+
 yay -Syyuu
 sudo reflector -c Hungary -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
+# basics
+
 install pacman $HOME/.dotfiles/res/pkglist/pkglist.txt
 install yay $HOME/.dotfiles/res/pkglist/pkglist_aur.txt
+
+# sway
+
+install pacman $HOME/.dotfiles/res/pkglist/pkglist_sway.txt
+sudo pacman -R xdg-desktop-portal-gnome
 
 chmod +x cleanup.sh
 chmod +x link.sh
--- scripts/link.sh
+++ scripts/link.sh
@@ -4,13 +4,11 @@
 
 rm -rf ~/{.ghc,.gitconfig,.zshenv}
 sudo rm -rf /etc/systemd/{logind.conf.d,sleep.conf.d}
-sudo rm -f /etc/X11/xorg.conf.d/30-touchpad.conf
-rm -rf ~/.config/{picom,kitty,nvim,ranger,dunst,zathura,zsh}
+rm -rf ~/.config/{picom,kitty,nvim,ranger,dunst,zathura,zsh,sway}
 
 # Create symlinks
 ln -s ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/ghc ~/.ghc
-sudo ln -s ~/.dotfiles/x/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo cp -r ~/.dotfiles/systemd/{logind.conf.d,sleep.conf.d} /etc/systemd # systemd can't see symlinks to ~ by default
-ln -s ~/.dotfiles/{picom,kitty,nvim,ranger,dunst,zathura,zsh} ~/.config
+ln -s ~/.dotfiles/{picom,kitty,nvim,ranger,dunst,zathura,zsh,sway} ~/.config
