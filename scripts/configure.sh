eb18e07f95e310f45dd7a9e1a4db6656d0d64572
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2024-02-10 21:09:11 +0100

    -

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo systemctl enable ly.service             # Display manager
-sudo systemctl enable betterlockscreen@$USER # Lockscreen
+sudo systemctl enable betterlockscreen # Lockscreen
 betterlockscreen -u $HOME/.dotfiles/res/wallpaper.jpg
--- scripts/link.sh
+++ scripts/link.sh
@@ -4,6 +4,7 @@
 
 rm -rf ~/{.ghc,.gitconfig,.zshenv}
 sudo rm -rf /etc/systemd/{logind.conf.d,sleep.conf.d}
+sudo rm -rf /etc/systemd/system/{betterlockscreen.service,betterlockscreen@.service}
 sudo rm -rf /etc/X11/xorg.conf.d
 rm -rf ~/.config/{picom,kitty,nvim,dunst,zathura,zsh,i3}
 
@@ -12,5 +13,6 @@ ln -s ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/ghc ~/.ghc
 sudo cp -r ~/.dotfiles/systemd/{logind.conf.d,sleep.conf.d} /etc/systemd # systemd can't see symlinks to ~ by default
+sudo ln -s ~/.dotfiles/systemd/system/betterlockscreen.service /usr/lib/systemd/system/betterlockscreen.service
 sudo ln -s ~/.dotfiles/x /etc/X11/xorg.conf.d
 ln -s ~/.dotfiles/{picom,kitty,nvim,dunst,zathura,zsh,i3} ~/.config
