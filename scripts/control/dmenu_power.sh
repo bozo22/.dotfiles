ba79d80ff2e349b2b063c4ed2c8f5912ffe32eb4
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2024-12-21 00:17:24 +0100

    -

--- scripts/control/dmenu_power.sh
+++ scripts/control/dmenu_power.sh
@@ -4,7 +4,7 @@ case "$(echo -e "Poweroff\nReboot\nWindows\nExit\nLock\nSuspend" | dmenu \
     -i -fn "$1")" \
     in
         Poweroff) exec systemctl poweroff;;
-        Reboot) exec systemctl reboot;;
+        Reboot) sudo grub-reboot 0 && reboot;;
         Windows) sudo grub-reboot 1 && reboot;;
         Exit) exec i3 exit;;
         Lock) exec betterlockscreen -l dim --off 300 --show-layout;;
