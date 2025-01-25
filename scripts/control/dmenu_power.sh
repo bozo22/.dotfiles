e993d0d5a763c5ec302f3018b37bd56e103d03d2
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2025-01-25 11:19:45 +0100

    -

--- scripts/control/dmenu_power.sh
+++ scripts/control/dmenu_power.sh
@@ -4,8 +4,8 @@ case "$(echo -e "Poweroff\nReboot\nWindows\nExit\nLock\nSuspend" | dmenu \
     -i -fn "$1")" \
     in
         Poweroff) exec systemctl poweroff;;
-        Reboot) sudo grub-reboot 0 && systemctl reboot;;
-        Windows) sudo grub-reboot 1 && systemctl reboot;;
+        Reboot) sudo grub-reboot 0 && reboot;;
+        Windows) sudo grub-reboot 1 && reboot;;
         Exit) exec i3 exit;;
         Lock) exec betterlockscreen -l dim --off 300 --show-layout;;
         Suspend) exec systemctl suspend-then-hibernate;;
